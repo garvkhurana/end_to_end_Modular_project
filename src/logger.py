6e7dafe2cbf776b8409e543dd346ff364b4c9a32
Garv Khurana <146458741+garvkhurana@users.noreply.github.com>
2025-02-25 17:03:29 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -2,21 +2,13 @@ import logging
 import os
 from datetime import datetime
 
+log_file = datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + ".log"
+log_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_dir, exist_ok=True)
 
-log_file=f"(datetime.now().strftime('%Y-%m-%d-%H-%M-%S')).log"
-log_path=os.path.join(os.getcwd(),"logs",log_file)
-os.makedirs(log_path,exist_ok=True)
+log_file_path = os.path.join(log_dir, log_file)
 
-log_file_path=os.path.join(log_path,log_file)   
+logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
-logging.basicConfig(filename=log_file_path,level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     logging.info('This is a test log message')
-    
-
-
-
-
