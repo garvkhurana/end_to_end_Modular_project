ab69875e14690f52343f59641169ec115a7933d1
Garv Khurana <146458741+garvkhurana@users.noreply.github.com>
2025-02-25 16:28:02 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,13 @@ import numpy as np
 import pandas as pd
 from src.exception import Customexception
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import r2_score    
+from sklearn.metrics import r2_score   
+import yaml
+from src.logging import logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from src.exception import Customexception
 
 def save_object(file_path, obj):
     try:
@@ -47,3 +53,53 @@ def load_object(file_path):
         
      except Exception as e:
         raise Customexception(e, sys)
+
+
+
+import os
+import yaml
+from src.logging import logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+import sys
+from src.exception import Customexception
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns . type key value pairs
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    
+    except Exception as e:
+        raise Customexception(e,sys)
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+
